711f18b7f47b092619339cc7b920f8ad451b1d38
Temich93s <nil>
2021-10-12 09:16:18 +1000

    Пройдена тема: Проверка соответствия протоколу

--- Protocols/main.swift
+++ Protocols/main.swift
@@ -395,3 +395,45 @@ func beginConcert(in location: Location & Named) {
 let seattle = City(name: "Seattle", latitude: 47.6, longitude: -122.3)
 beginConcert(in: seattle)
 // Prints "Hello, Seattle!"
+
+
+//MARK: Проверка соответствия протоколу
+print("\n//Проверка соответствия протоколу")
+
+protocol HasArea {
+    var area: Double { get }
+}
+
+class Circle: HasArea {
+    let pi = 3.1415927
+    var radius: Double
+    var area: Double { return pi * radius * radius }
+    init(radius: Double) { self.radius = radius }
+}
+
+class Country: HasArea {
+    var area: Double
+    init(area: Double) { self.area = area }
+}
+
+class Animal {
+    var legs: Int
+    init(legs: Int) { self.legs = legs }
+}
+
+let objects: [AnyObject] = [
+    Circle(radius: 2.0),
+    Country(area: 243_610),
+    Animal(legs: 4)
+]
+
+for object in objects {
+    if let objectWithArea = object as? HasArea {
+        print("Area is \(objectWithArea.area)")
+    } else {
+        print("Something that doesn't have an area")
+    }
+}
+// Area is 12.5663708
+// Area is 243610.0
+// Something that doesn't have an area

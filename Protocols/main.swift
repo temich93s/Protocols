2136a94c1f92c13ddd23179cd78f007ab19428a9
Temich93s <nil>
2021-10-11 11:02:46 +1000

    Пройдена тема: Требование свойств

--- Protocols/main.swift
+++ Protocols/main.swift
@@ -9,3 +9,34 @@ import Foundation
 
 print("Hello, World!")
 
+
+//MARK: Требование свойств
+print("\n//Требование свойств")
+
+protocol FullyNamed {
+    var fullName: String { get }
+}
+
+struct Person: FullyNamed {
+    var fullName: String
+}
+
+let john = Person(fullName: "John Appleseed")
+print(john)
+
+class Starship: FullyNamed {
+    var prefix: String?
+    var name: String
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+    var fullName: String {
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+}
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+print(ncc1701.fullName)
+// ncc1701.fullName равен "USS Enterprise"
+
+

48a9b73c87a9a9fb888917d9022b5069ef9f949f
Temich93s <nil>
2021-10-11 11:11:19 +1000

    Пройдена тема: Требование методов

--- Protocols/main.swift
+++ Protocols/main.swift
@@ -40,3 +40,26 @@ print(ncc1701.fullName)
 // ncc1701.fullName равен "USS Enterprise"
 
 
+//MARK: Требование методов
+print("\n//Требование методов")
+
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c)
+            .truncatingRemainder(dividingBy:m))
+        return lastRandom / m
+    }
+}
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+// Prints "Here's a random number: 0.3746499199817101"
+print("And another one: \(generator.random())")
+// Prints "And another one: 0.729023776863283"
